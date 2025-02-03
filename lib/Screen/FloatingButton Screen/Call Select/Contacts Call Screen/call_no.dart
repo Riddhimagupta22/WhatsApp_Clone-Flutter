44c0c9a5d9b3f514b62b187585ff609e2a791aad
Riddhima Gupta <griddhima226@gmail.com>
2025-02-03 22:32:00 +0530

    commit

--- lib/Screen/FloatingButton Screen/Call Select/Contacts Call Screen/call_no.dart
+++ lib/Screen/FloatingButton Screen/Call Select/Contacts Call Screen/call_no.dart
@@ -39,7 +39,7 @@ class CallNo extends StatelessWidget {
              padding: const EdgeInsets.symmetric(vertical: 20.0),
              child: ListTile(
                leading: CircleAvatar(
-                 radius: 25,
+                 radius: 26,
                  backgroundColor:Colors.green ,
                  child: Icon(Icons.videocam_outlined,color: Colors.black,size: 28,),
                ),
